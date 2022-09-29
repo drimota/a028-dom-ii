1ad7f638c74137c7f38e123022c172f4a14b07c2
drimota <driismota@hotmail.com>
2022-09-29 09:12:00 -0400

    DOM II

---
+++ script.js
@@ -0,0 +1,14 @@
+function adicionaItem(event) {
+    const caixa = document.getElementById('container')
+    const newArticle = document.createElement('article')
+    const newItem = document.createTextNode('Novo Item')
+    newArticle.appendChild(newItem)
+    caixa.insertAdjacentElement('beforeend', newArticle)
+
+
+    newelement.setAttribute('class', 'item')
+}
+
+function removeItem(event) {
+    event.target.remove()
+}
